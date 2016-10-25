0b37a6dd9f68de767cccaf6453de49832ab3df35
Carlos <itscarlos@users.noreply.github.com>
2016-10-25 21:59:56 +0200

    Update install-upgrade_py_module.cmd

--- install-upgrade_py_module.cmd
+++ install-upgrade_py_module.cmd
@@ -1,6 +1,10 @@
 @echo off
-::https://github.com/itscarlos
+set year=%date:~6,4% 
+set month=%date:~3,2% 
+set day=%date:~0,2% 
+set hour=%time:~0,2% 
+:a
 set /p module=python module: 
 pip install --upgrade %module%
-pause>nul
-exit
+echo %day%/%month%/%year%(%hour%h) module: %module%>>modules.txt
+goto a
